db3e1a88989a66a65dae10c6231abaebb8fc84f2
cheichey <cheichey.1223@gmail.com>
2021-06-15 11:57:52 +0900

    feat: timerを追加

---
+++ Timer/Timer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        int endTime;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            endTime = int.Parse(textBox1.Text);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox2.Text = endTime.ToString();
+            endTime--;
+            if (endTime == 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("時間になりました。");
+            }
+           
+        }
+    }
+}
